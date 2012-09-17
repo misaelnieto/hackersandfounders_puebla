2634be01b417cafceeb0bfd376cbc80b6586e6a1
Noe Dominguez <zeugop@gmail.com>
2012-09-17 17:05:30 -0500

    Migrated to Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackersandfoundersPuebla::Application.config.secret_token = '0a63c34b4feb9385638c373402ed940b5995766d3c07d34f9420f1ee20955714fe7a468fae6abe9a1c8549eb66f9e9be828872db3e6c2a8cacf732064d251423'
